cfad344bba7c946f9567fa81e856df378307502a
Helder Vieira <vieiraa360@gmail.com>
2018-05-09 22:40:32 +0000

    updated testimonial posts

--- testimonials/models.py
+++ testimonials/models.py
@@ -9,5 +9,5 @@ class Testimonial(models.Model):
     author = models.CharField(max_length=100)
     affiliation = models.CharField(max_length=100, blank=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
-    image = models.ImageField(upload_to="img", blank=True, null=True)
+    image = models.ImageField(upload_to="media/img", blank=True, null=True)
     active = models.BooleanField(default=False)
