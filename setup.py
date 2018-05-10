da1b11aabd4da397a274dcbdb63b529ddb68ed6a
Helder Vieira <vieiraa360@gmail.com>
2018-05-10 23:23:42 +0000

    update

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 
 setup(name='stream3_project',
       version='1.0',
-      packages=find_packages())
+      packages=find_packages(),
+      scripts=['manage.py'])
