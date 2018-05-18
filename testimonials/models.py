b4852a8ea57a2efd9b705d9e3ca949c168b5703b
Helder Vieira <vieiraa360@gmail.com>
2018-05-18 22:23:37 +0000

    update

--- testimonials/models.py
+++ testimonials/models.py
@@ -8,7 +8,7 @@ class Testimonial(models.Model):
 
     title = models.CharField(max_length=50, null=True)
     text = models.TextField()
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     affiliation = models.CharField(max_length=100, blank=True)
     added = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
