9e020075703d5999db7f52cb92e0809d6aae5443
Helder Vieira <vieiraa360@gmail.com>
2018-05-18 11:14:17 +0000

    updated travis yml file

--- posts/forms.py
+++ posts/forms.py
@@ -6,6 +6,6 @@ from .models import Post
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+        fields = ('title', 'author', 'content', 'image', 'tag', 'published_date')
 
 
