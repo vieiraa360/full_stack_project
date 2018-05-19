9bb7000b2e9d25bdeccf538e4c35d97cc7a15bf8
Helder Vieira <vieiraa360@gmail.com>
2018-05-19 12:22:21 +0000

    update to author field

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -102,6 +102,10 @@ else:
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3', 
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'TEST': {
+		    'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test_db.sqlite3',
+        },
 		}
 	}
 
