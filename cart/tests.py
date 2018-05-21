919de4442193ca74eea92d55e0e5859b0495a186
Helder Vieira <vieiraa360@gmail.com>
2018-05-21 22:32:00 +0000

    tests

--- cart/tests.py
+++ cart/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.test import Client
 from django.conf.urls import url
-from .views import adjust_cart
 # Create your tests here.
 
 
@@ -9,7 +8,9 @@ class Cart_Url_Test(TestCase):
     def test_details(self):
         response = self.client.get('/cart/')
         self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart.html')
         
     def test_index(self):
             response = self.client.get('/')
             self.assertTrue(response.status_code, 200)
+            self.assertTemplateUsed(response, 'index.html')
--- testimonials/tests.py
+++ testimonials/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.test import Client
+from django.conf.urls import url
 from django.http import HttpRequest
-from django.test import SimpleTestCase
 from django.urls import reverse
 from .models import Testimonial
 from .views import testimonial_detail, get_testimonials
@@ -9,11 +10,11 @@ from .views import testimonial_detail, get_testimonials
 
 class TestimonialsPageTests(TestCase):
     def test_view_uses_correct_template(self):
-        response = self.client.get('get_testimonials')
-        self.assertEquals(response.status_code, 200)
+        response = self.client.get('/testimonials/')
+        self.assertTrue(response.status_code, 200)
         self.assertTemplateUsed(response, 'testimonials.html')
         
     def test_view_url_by_name(self):
-        response = self.client.get('testimonial_detail')
-        self.assertEquals(response.status_code, 200)
+        response = self.client.get('/testimonial_detail/')
+        self.assertTrue(response.status_code, 200)
         self.assertTemplateUsed(response, 'testimonialpost.html')
