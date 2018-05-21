0f054242c7316e88a59b7ec6a8a0ee393a4e716d
Helder Vieira <vieiraa360@gmail.com>
2018-05-21 22:16:53 +0000

    tests

--- testimonials/tests.py
+++ testimonials/tests.py
@@ -2,18 +2,18 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.test import SimpleTestCase
 from django.urls import reverse
+from .models import Testimonial
+from .views import testimonial_detail, get_testimonials
 
 
 
-class TestimonialsPageTests(SimpleTestCase):
+class TestimonialsPageTests(TestCase):
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('get_testimonials'))
+        response = self.client.get('get_testimonials')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'testimonials.html')
         
     def test_view_url_by_name(self):
-        response = self.client.get(reverse('testimonial_detail'))
+        response = self.client.get('testimonial_detail')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'testimonialpost.html')
-
-
