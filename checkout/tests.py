2ece4260ed9940f2f79ad26a99e58f4e3a505365
Helder Vieira <vieiraa360@gmail.com>
2018-06-03 15:32:02 +0000

    avatar default image changed

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class Checkout_Url_Test(TestCase):
+    def test_details(self):
+        response = self.client.get('/checkout/')
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout.html')
